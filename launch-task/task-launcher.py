ce1cefeaa79731e2c802fe6c6aebadebfd572fb5
langdon <langdon@fedoraproject.org>
2016-01-01 16:37:53 -0500

    adding some updates to 'task context launcher'

--- launch-task/task-launcher.py
+++ launch-task/task-launcher.py
@@ -4,12 +4,15 @@ import argparse
 import os
 
 parser = argparse.ArgumentParser(description='launch a task')
-parser.add_argument('projects_root', metavar='projects-root', nargs='?', default="", help='root directory of status files')
-parser.add_argument('task_name', metavar='task-name', nargs='?', default="", help='name of the task to launch')
+#parser.add_argument('projects_root', metavar='projects-root', nargs='?', default="", help='root directory of status files')
+#parser.add_argument('task_name', metavar='task-name', nargs='?', default="", help='name of the task to launch')
+parser.add_argument('task_file', metavar='task-file', nargs='?', default="", help='name of the file with task information to launch')
 
 args = parser.parse_args()
 print(dir(args))
-path_to_task = os.path.join(args.projects_root, args.task_name, 'status.txt')
+#path_to_task = os.path.join(args.projects_root, args.task_name, 'status.txt')
+path_to_task = args.task_file
+
 print("task: %s" % path_to_task)
 with open (path_to_task, 'r') as status_file:
     content = status_file.read()
